89b6c73ea7209826a6c33ff8b2e86988ddc4315d
Yuvan Shankar Raja <yuvan7480@gmail.com>
2024-01-12 12:29:06 +0530

    sdfghjkl

---
+++ java/test.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class test {
+    int a[][];
+    int i,j,m,n;
+    Scanner S=new Scanner(System.in);
+    
+    for(i=0;i<m;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            a[i][j]=S.nextInt();
+        }
+    }
+}
