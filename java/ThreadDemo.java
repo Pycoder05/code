612e436e69ab32bd072f24512e38be625167004c
Yuvan Shankar Raja <yuvan7480@gmail.com>
2024-04-10 11:39:51 +0530

    main

--- java/ThreadDemo.java
+++ java/ThreadDemo.java
@@ -9,7 +9,7 @@ public class ThreadDemo{
         for(int i=5;i>0;i--){
             System.out.println(i);
             try{
-                Thread.sleep(1000,6000);
+                Thread.sleep(1000);
             }
             catch(InterruptedException e){
                 System.out.println("main thread Interrupted");
